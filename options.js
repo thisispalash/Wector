41f38a480456f84cee93c3f84513350d88b5f4ea
Karun Singh <karun.singh17@gmail.com>
2015-07-03 19:37:35 +0530

    UI is done, looks nice

--- options.js
+++ options.js
@@ -8,10 +8,16 @@ var lon = -76.5018807;
 var loc = "Ithaca, NY";
 
 function refreshMap() {
+	document.getElementById("map").style.opacity = "0.5";
 	var address = document.getElementById("whereAmIInput").value;
-	// set lat lon and loc
-	console.log(address);
-	document.getElementById("map").src = "https://maps.googleapis.com/maps/api/staticmap?center="+address+"&zoom=13&size=600x300&maptype=roadmap&markers=color:orange|"+address;
+	var append = lat+","+lon;
+	if (address != 'Current Location') {
+		append = address;
+	}	
+	document.getElementById("map").src = "https://maps.googleapis.com/maps/api/staticmap?center="+append+"&zoom=13&size=600x300&maptype=roadmap&markers=color:orange|"+append;
+	document.getElementById("map").onload = function () {
+		document.getElementById("map").style.opacity = "1.0";
+	};
 }
 
 // Find current location that has been set, and send it over to the map.
@@ -19,6 +25,25 @@ function preInitialize() {
 	// retrieve lat lon loc
 	document.getElementById("whereAmIInput").value = loc;
 	document.getElementById("submit").addEventListener("click",refreshMap);
+	document.getElementById("form").addEventListener("submit",function (event) {
+		event.preventDefault();
+		refreshMap();
+	});
+	getLocation();
+	refreshMap();
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+    }
+}
+function showPosition(position) {
+	lat = position.coords.latitude;
+	lon = position.coords.longitude;
+	console.log(position.coords);
+	document.getElementById("whereAmIInput").value = "Current Location";
 	refreshMap();
 }
 
