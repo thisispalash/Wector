09837f47e80edbba427c358aa20629774d4b4b8e
Karun Singh <karun.singh17@gmail.com>
2015-07-03 23:12:52 +0530

    Added 'Saved' alert to the options page

--- options.js
+++ options.js
@@ -9,15 +9,20 @@ var loc = "Ithaca, NY";
 
 function refreshMap() {
 	document.getElementById("map").style.opacity = "0.5";
-	var address = document.getElementById("whereAmIInput").value;
-	var append = lat+","+lon;
-	if (address != 'Current Location') {
-		append = address;
-	}	
-	document.getElementById("map").src = "https://maps.googleapis.com/maps/api/staticmap?center="+append+"&zoom=13&size=600x300&maptype=roadmap&markers=color:orange|"+append;
+	var address = document.getElementById("whereAmIInput").value;	
+	document.getElementById("map").src = "https://maps.googleapis.com/maps/api/staticmap?center="+address+"&zoom=13&size=600x300&maptype=roadmap&markers=color:orange|"+address;
 	document.getElementById("map").onload = function () {
 		document.getElementById("map").style.opacity = "1.0";
 	};
+}
+
+function save() {
+	document.getElementById("save").innerHTML = 'Saving <i class="fa fa-spinner fa-spin"></i>';
+	var address = document.getElementById("whereAmIInput").value;
+	document.getElementById("save").innerHTML = "Save";
+	var a = document.getElementById("saveAlert");
+	$(a).fadeIn();
+	setInterval(function(){ $(a).fadeOut() }, 5000);
 }
 
 // Find current location that has been set, and send it over to the map.
@@ -25,25 +30,11 @@ function preInitialize() {
 	// retrieve lat lon loc
 	document.getElementById("whereAmIInput").value = loc;
 	document.getElementById("submit").addEventListener("click",refreshMap);
+	document.getElementById("save").addEventListener("click",save);
 	document.getElementById("form").addEventListener("submit",function (event) {
 		event.preventDefault();
 		refreshMap();
 	});
-	getLocation();
-	refreshMap();
-}
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-    }
-}
-function showPosition(position) {
-	lat = position.coords.latitude;
-	lon = position.coords.longitude;
-	console.log(position.coords);
-	document.getElementById("whereAmIInput").value = "Current Location";
 	refreshMap();
 }
 
