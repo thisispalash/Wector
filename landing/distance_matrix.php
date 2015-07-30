75b5c961f234e4bfee59b649020953c9ce5eecbd
Karun Singh <karun.singh17@gmail.com>
2015-07-30 18:58:33 +0530

    Product Hunt release updates to the landing page

--- landing/distance_matrix.php
+++ landing/distance_matrix.php
@@ -3,5 +3,7 @@
 	$lat = $_GET['lat'];
 	$lon = $_GET['lon'];
 	$dst = $_GET['dst'];
-	echo json_encode(file_get_contents("https://maps.googleapis.com/maps/api/distancematrix/json?origins=".$lat.",".$lon."&destinations=".$dst."&language=en-EN&key=AIzaSyAaZ_ZPlWIX0AiF7iIrN27EMfUupJiIPJo"));
+	$keys = ['AIzaSyAaZ_ZPlWIX0AiF7iIrN27EMfUupJiIPJo','AIzaSyBcSybt87J4yc9kxf3--QTMFPWIatRVOzM','AIzaSyCaICvBDrbLQLVm5-4Ot-qj4nE2AxZ3RQ4'];
+	$key = $keys[rand(0,2)];
+	echo json_encode(file_get_contents("https://maps.googleapis.com/maps/api/distancematrix/json?origins=".$lat.",".$lon."&destinations=".$dst."&language=en-EN&key=".$key));
 ?>
--- landing/geocode.php
+++ landing/geocode.php
@@ -1,5 +1,7 @@
 <?php
 	header("Content-Type:application/json");
 	$dst = $_GET['dst'];
-	echo json_encode(file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address=".$dst."&key=AIzaSyAaZ_ZPlWIX0AiF7iIrN27EMfUupJiIPJo"));
+	$keys = ['AIzaSyAaZ_ZPlWIX0AiF7iIrN27EMfUupJiIPJo','AIzaSyBcSybt87J4yc9kxf3--QTMFPWIatRVOzM','AIzaSyCaICvBDrbLQLVm5-4Ot-qj4nE2AxZ3RQ4'];
+	$key = $keys[rand(0,2)];
+	echo json_encode(file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address=".$dst."&key=".$key));
 ?>
